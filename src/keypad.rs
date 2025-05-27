7e443baea5feff372e812ef893046e173e0d79ad
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2025-05-27 21:52:56 +0200

    :sparkles: Create module to monitor keypad

---
+++ src/keypad.rs
@@ -0,0 +1,206 @@
+use embedded_hal::digital::{InputPin, OutputPin, PinState};
+
+pub struct Keypad4x4<T, U>
+where
+    T: InputPin,
+    U: OutputPin,
+{
+    rows: [T; 4],
+    columns: [U; 4],
+}
+
+impl<T, U> Keypad4x4<T, U>
+where
+    T: InputPin,
+    U: OutputPin,
+{
+    pub fn new(mut rows: [T; 4], mut columns: [U; 4]) -> Keypad4x4<T, U> {
+        if rows.iter_mut().any(|row| row.is_high().unwrap()) {
+            panic!("Input pins should be pulled low");
+        }
+        columns
+            .iter_mut()
+            .for_each(|output| output.set_low().unwrap());
+        Self { rows, columns }
+    }
+
+    pub fn key_0(&mut self) -> PinState {
+        self.check_key_state(Key::Num0)
+    }
+
+    pub fn key_1(&mut self) -> PinState {
+        self.check_key_state(Key::Num1)
+    }
+
+    pub fn key_2(&mut self) -> PinState {
+        self.check_key_state(Key::Num2)
+    }
+
+    pub fn key_3(&mut self) -> PinState {
+        self.check_key_state(Key::Num3)
+    }
+
+    pub fn key_4(&mut self) -> PinState {
+        self.check_key_state(Key::Num4)
+    }
+
+    pub fn key_5(&mut self) -> PinState {
+        self.check_key_state(Key::Num5)
+    }
+
+    pub fn key_6(&mut self) -> PinState {
+        self.check_key_state(Key::Num6)
+    }
+
+    pub fn key_7(&mut self) -> PinState {
+        self.check_key_state(Key::Num7)
+    }
+
+    pub fn key_8(&mut self) -> PinState {
+        self.check_key_state(Key::Num8)
+    }
+
+    pub fn key_9(&mut self) -> PinState {
+        self.check_key_state(Key::Num9)
+    }
+
+    pub fn key_a(&mut self) -> PinState {
+        self.check_key_state(Key::A)
+    }
+
+    pub fn key_b(&mut self) -> PinState {
+        self.check_key_state(Key::B)
+    }
+
+    pub fn key_c(&mut self) -> PinState {
+        self.check_key_state(Key::C)
+    }
+
+    pub fn key_d(&mut self) -> PinState {
+        self.check_key_state(Key::D)
+    }
+
+    pub fn key_star(&mut self) -> PinState {
+        self.check_key_state(Key::Star)
+    }
+
+    pub fn key_pound(&mut self) -> PinState {
+        self.check_key_state(Key::Pound)
+    }
+
+    fn check_key_state(&mut self, key: Key) -> PinState {
+        let key_position = key.get_indexes();
+        self.columns
+            .get_mut(key_position.column_index)
+            .expect("Invalid column index")
+            .set_high()
+            .unwrap();
+        let is_pressed = self
+            .rows
+            .get_mut(key_position.row_index)
+            .expect("Invalid row index")
+            .is_high()
+            .unwrap();
+        self.columns
+            .get_mut(key_position.column_index)
+            .expect("Invalid column index")
+            .set_low()
+            .unwrap();
+        is_pressed.into()
+    }
+}
+
+struct KeyPosition {
+    row_index: usize,
+    column_index: usize,
+}
+
+enum Key {
+    Num0,
+    Num1,
+    Num2,
+    Num3,
+    Num4,
+    Num5,
+    Num6,
+    Num7,
+    Num8,
+    Num9,
+    A,
+    B,
+    C,
+    D,
+    Star,
+    Pound,
+}
+
+impl Key {
+    fn get_indexes(self) -> KeyPosition {
+        match self {
+            Key::Num1 => KeyPosition {
+                row_index: 0,
+                column_index: 0,
+            },
+            Key::Num2 => KeyPosition {
+                row_index: 0,
+                column_index: 1,
+            },
+            Key::Num3 => KeyPosition {
+                row_index: 0,
+                column_index: 2,
+            },
+            Key::A => KeyPosition {
+                row_index: 0,
+                column_index: 3,
+            },
+            Key::Num4 => KeyPosition {
+                row_index: 1,
+                column_index: 0,
+            },
+            Key::Num5 => KeyPosition {
+                row_index: 1,
+                column_index: 1,
+            },
+            Key::Num6 => KeyPosition {
+                row_index: 1,
+                column_index: 2,
+            },
+            Key::B => KeyPosition {
+                row_index: 1,
+                column_index: 3,
+            },
+            Key::Num7 => KeyPosition {
+                row_index: 2,
+                column_index: 0,
+            },
+            Key::Num8 => KeyPosition {
+                row_index: 2,
+                column_index: 1,
+            },
+            Key::Num9 => KeyPosition {
+                row_index: 2,
+                column_index: 2,
+            },
+            Key::C => KeyPosition {
+                row_index: 2,
+                column_index: 3,
+            },
+            Key::Star => KeyPosition {
+                row_index: 3,
+                column_index: 0,
+            },
+            Key::Num0 => KeyPosition {
+                row_index: 3,
+                column_index: 1,
+            },
+            Key::Pound => KeyPosition {
+                row_index: 3,
+                column_index: 2,
+            },
+            Key::D => KeyPosition {
+                row_index: 3,
+                column_index: 3,
+            },
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,79 @@
 #![no_std]
 #![no_main]
 
+mod keypad;
+
+use defmt::info;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
+use embassy_time::{Timer};
 use {defmt_rtt as _, panic_probe as _};
 
+use crate::keypad::Keypad4x4;
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) -> ! {
-    let _p = embassy_stm32::init(Default::default());
+async fn main(spawner: Spawner) {
+    info!("Start main");
+    let p = embassy_stm32::init(Default::default());
+
+    let rows = [
+        Input::new(p.PA0, Pull::Down),
+        Input::new(p.PA1, Pull::Down),
+        Input::new(p.PA2, Pull::Down),
+        Input::new(p.PA3, Pull::Down),
+    ];
+
+    let columns = [
+        Output::new(p.PA4, Level::Low, Speed::Low),
+        Output::new(p.PA5, Level::Low, Speed::Low),
+        Output::new(p.PC4, Level::Low, Speed::Low),
+        Output::new(p.PA7, Level::Low, Speed::Low),
+    ];
+
+    info!("Create keypad");
+    let keypad = Keypad4x4::new(rows, columns);
+
+    spawner.spawn(monitor_keypad(keypad)).unwrap();
+}
+
+#[embassy_executor::task]
+async fn monitor_keypad(mut keypad: Keypad4x4<Input<'static>, Output<'static>>) {
     loop {
-        defmt::info!("Blink");
-        Timer::after(Duration::from_millis(100)).await;
+        let key_1: bool = keypad.key_1().into();
+        let key_2: bool = keypad.key_2().into();
+        let key_3: bool = keypad.key_3().into();
+        let key_a: bool = keypad.key_a().into();
+        let key_4: bool = keypad.key_4().into();
+        let key_5: bool = keypad.key_5().into();
+        let key_6: bool = keypad.key_6().into();
+        let key_b: bool = keypad.key_b().into();
+        let key_7: bool = keypad.key_7().into();
+        let key_8: bool = keypad.key_8().into();
+        let key_9: bool = keypad.key_9().into();
+        let key_c: bool = keypad.key_c().into();
+        let key_star: bool = keypad.key_star().into();
+        let key_0: bool = keypad.key_0().into();
+        let key_pound: bool = keypad.key_pound().into();
+        let key_d: bool = keypad.key_d().into();
+        info!(
+            "\nkey A: {}\nkey B: {}\nkey C: {}\nkey D: {}\nkey *: {}\nkey #: {}\nkey 0: {}\nkey 1: {}\nkey 2: {}\nkey 3: {}\nkey 4: {}\nkey 5: {}\nkey 6: {}\nkey 7: {}\nkey 8: {}\nkey 9: {}",
+            key_a,
+            key_b,
+            key_c,
+            key_d,
+            key_star,
+            key_pound,
+            key_0,
+            key_1,
+            key_2,
+            key_3,
+            key_4,
+            key_5,
+            key_6,
+            key_7,
+            key_8,
+            key_9,
+        );
+        Timer::after_millis(100).await;
     }
 }
